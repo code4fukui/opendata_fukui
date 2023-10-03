80aed3a57fd2256ba0e0926ed764014b56061aba
Taisuke Fukuno <fukuno@jig.jp>
2023-10-03 15:26:49 +0900

    add data of Tsuruga

--- README.md
+++ README.md
@@ -12,3 +12,7 @@
 
 - [RENEW Pay 決済オープンデータ](renewpay_payment_2022.csv) [CSV](https://code4fukui.github.io/opendata_fukui/renewpay_payment_2022.csv) (出展: [株式会社ふくいのデジタル](https://www.fukui-digital.co.jp/)＆[ふくいドットコム 福井県観光データ分析システム FTAS](https://www.fuku-e.com/feature/detail_266.html))
 - [RENEW Pay 決済オープンデータ(福井県観光アンケートオープンデータ会員ID突合データ)](renewpay_payment_linked_2022.csv) [CSV](https://code4fukui.github.io/opendata_fukui/renewpay_payment_linked_2022.csv) (出展: [株式会社ふくいのデジタル](https://www.fukui-digital.co.jp/)＆[ふくいドットコム 福井県観光データ分析システム FTAS](https://www.fuku-e.com/feature/detail_266.html))
+
+## 敦賀市の人口
+
+- [敦賀市の人口](tsuruga_population_statistics.csv) 出典: [敦賀市オープンデータ　敦賀市-Tsuruga City-](https://www.city.tsuruga.lg.jp/about_city/tokei_nenpo/opendata.html)
---
+++ deno/populationDownloadTsuruga.js
@@ -0,0 +1,22 @@
+import { CSV } from "https://js.sabae.cc/CSV.js";
+import { ArrayUtil } from "https://js.sabae.cc/ArrayUtil.js";
+
+const url = "http://linkdata.org/api/1/rdf1s3648i/population_statistics.csv";
+const data = await (await fetch(url)).text();
+await Deno.writeTextFile("../tsuruga_population_statistics.csv", data);
+//const data = await CSV.fetchJSON("../tsuruga_population_statistics.csv");
+
+// population_statistics,調査年,標準地域コード,町丁字コード,都道府県名,市区町村名,行政区名,大字・町名,字・丁目名,最少年齢,最大年齢,人口,男性人口,女性人口,備考
+// date,lgcode,population
+const list = [];
+const years = ArrayUtil.toUnique(data.map(d => d.調査年)).sort();
+for (const year of years) {
+  const p = data.filter(d => year == d.調査年 && !(d.最少年齢 == 0 && d.最大年齢 === "")).reduce((pre, cur) => pre += parseInt(cur.人口), 0);
+  const lgcode = data[0].標準地域コード;
+  list.push({
+    date: year + "-01-01",
+    lgcode,
+    population: p,
+  });
+}
+await Deno.writeTextFile("../tsuruga_population.csv", CSV.stringify(list));
