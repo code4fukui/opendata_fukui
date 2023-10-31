08d3a0ed5972c18695ed2c39d4b6567d6db4d159
Taisuke Fukuno <fukuno@jig.jp>
2023-11-01 07:33:01 +0900

    fix fetch

--- deno/populationDownloadTsuruga.js
+++ deno/populationDownloadTsuruga.js
@@ -2,8 +2,8 @@ import { CSV } from "https://js.sabae.cc/CSV.js";
 import { ArrayUtil } from "https://js.sabae.cc/ArrayUtil.js";
 
 const url = "http://linkdata.org/api/1/rdf1s3648i/population_statistics.csv";
-const data = await (await fetch(url)).text();
-await Deno.writeTextFile("../tsuruga_population_statistics.csv", data);
+const data = await CSV.fetchJSON(url);
+await Deno.writeTextFile("../tsuruga_population_statistics.csv", CSV.stringify(data));
 //const data = await CSV.fetchJSON("../tsuruga_population_statistics.csv");
 
 // population_statistics,調査年,標準地域コード,町丁字コード,都道府県名,市区町村名,行政区名,大字・町名,字・丁目名,最少年齢,最大年齢,人口,男性人口,女性人口,備考
