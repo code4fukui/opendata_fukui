acc09189c24ed4201867f346b3f60aca0b00f575
Taisuke Fukuno <fukuno@jig.jp>
2023-11-01 07:32:56 +0900

    remove debug out

--- deno/populationDownload.js
+++ deno/populationDownload.js
@@ -5,9 +5,9 @@ const base = "https://www.city.sabae.fukui.jp/about_city/tokeijoho/";
 const url = "https://www.city.sabae.fukui.jp/about_city/tokeijoho/sabae-jinko.html";
 //const html = await (await fetch(url)).text();
 const html = new TextDecoder().decode(await fetchCurl(url));
-console.log(html);
+//console.log(html);
 const dom = HTMLParser.parse(html);
-console.log(dom);
+//console.log(dom);
 const as = dom.querySelectorAll("a.xls");
 for (const a of as) {
   const name = a.getAttribute("href");
