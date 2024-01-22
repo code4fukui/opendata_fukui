8565fff274d60d0aec3785f7f7b125ad23b2bf46
Taisuke Fukuno <fukuno@jig.jp>
2024-01-22 09:47:17 +0900

    add population by areas

--- deno/populationXls2CSV.js
+++ deno/populationXls2CSV.js
@@ -5,6 +5,19 @@ import { dir2array } from "https://js.sabae.cc/dir2array.js";
 import { Day } from "https://js.sabae.cc/DateTime.js";
 import { CSV } from "https://js.sabae.cc/CSV.js";
 
+const areas = [
+  { area: "鯖江", pos: "F9" },
+  { area: "新横江", pos: "F13" },
+  { area: "神明", pos: "F17" },
+  { area: "中河", pos: "F21" },
+  { area: "片上", pos: "F25" },
+  { area: "立待", pos: "M9" },
+  { area: "吉川", pos: "M13" },
+  { area: "豊", pos: "M17" },
+  { area: "北中山", pos: "M21" },
+  { area: "河和田", pos: "F25" },
+];
+
 const path = "../org/sabae-jinko.files/";
 const files = await dir2array(path);
 const data = [];
@@ -30,7 +43,11 @@ for (const file of files) {
     if (data.find(d => d.date == date2)) {
       continue;
     }
-    data.push({ date: date2, lgcode: "182079", population: pop });
+    const d = { date: date2, lgcode: "182079", population: pop };
+    for (const area of areas) {
+      d[area.area] = sh[area.pos].v;
+    }
+    data.push(d);
   }
 }
 data.sort((a, b) => a.date.localeCompare(b.date));
